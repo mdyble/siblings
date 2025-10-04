65fac08a37088a35409fdaf5d74ba49bd7371443
mdyble <md479@cam.ac.uk>
2025-10-04 12:00:25 +0100

    Update exclusivity counter.R

--- exclusivity counter.R
+++ exclusivity counter.R
@@ -52,53 +52,7 @@ meta_data_kinsources # take a look
 
 
 
-
-### 2) for aDNA
-
-# set working directory to folder with the aDNA parentage datasets in 
-
-items <- list.files() # List of the datasets
-n_inds = exclusive_inds = non_excl_inds = rep(NA,length(items)) # empty vectors to store information in
-
-for(z in 1:length(items)){ # loop through each dataset
-  item = items[z] # pull out the name of the current dataset 
-  dataset <- read.csv(item) # import that dataset 
-  
-  mothers = unique(dataset$Mother)
-  fathers = unique(dataset$Father)
-  
-  ## Only include those in the sample if they are in the ID column too (and therefore were actually sampled rather than genetically inferred)
-  mothers = mothers[which(mothers%in%dataset$ID)]
-  fathers = fathers[which(fathers%in%dataset$ID)]
-  
-  excl = nonexcl = 0
-  
-  for(i in 1:length(mothers)){
-  partners = unique(dataset$Father[which(dataset$Mother==mothers[i])])
-  n_partners = length(partners)
-  n_partners_of_partner = length(unique(dataset$Mother[which(dataset$Father%in%partners)]))
-  if(n_partners==1 & n_partners_of_partner==1) excl = excl+1 else nonexcl=nonexcl+1 
-  }
-  
-  for(i in 1:length(fathers)){
-    partners = unique(dataset$Mother[which(dataset$Father==fathers[i])])
-    n_partners = length(partners)
-    n_partners_of_partner = length(unique(dataset$Father[which(dataset$Mother%in%partners)]))
-    if(n_partners==1 & n_partners_of_partner==1) excl = excl+1 else nonexcl=nonexcl+1 
-  }
-  exclusive_inds[z] = excl
-  non_excl_inds[z] = nonexcl
-}
-meta_data_aDNA = cbind.data.frame(items,exclusive_inds,non_excl_inds)
-meta_data_aDNA
-
-## Only include ipopulations with >10 individuals included 
-meta_data_aDNA = meta_data_aDNA[which(exclusive_inds+non_excl_inds>10),]
-
-
-
-
-### 3) for mammals
+### 2) for mammals
 # set working directory to folder with the aDNA parentage datasets in 
 
 items <- list.files() # List of the datasets
@@ -140,13 +94,11 @@ meta_data_mammals_nonmonog = meta_data_mammals[-c(2,3,7,16,19),]
 
 ####
 ethno_props = (meta_data_kinsources$exclusive_inds/(meta_data_kinsources$exclusive_inds+meta_data_kinsources$non_excl_inds))*100
-aDNA_props = (meta_data_aDNA$exclusive_inds/(meta_data_aDNA$exclusive_inds+meta_data_aDNA$non_excl_inds))*100
 mono_mammal_props = (meta_data_mammals_monog$exclusive_inds/(meta_data_mammals_monog$exclusive_inds+meta_data_mammals_monog$non_excl_inds))*100
 nonmono_mammal_props = (meta_data_mammals_nonmonog$exclusive_inds/(meta_data_mammals_nonmonog$exclusive_inds+meta_data_mammals_nonmonog$non_excl_inds))*100
 
 ## summarise
 mean(ethno_props);length(ethno_props)
-mean(aDNA_props);length(aDNA_props)
 mean(mono_mammal_props);length(mono_mammal_props)
 mean(nonmono_mammal_props);length(nonmono_mammal_props)
 
